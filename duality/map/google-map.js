b29ad0d0042fef4e50aeaa79eb83fad34092dea2
Justin Lengvarsky <justin.lengvarsky@gmail.com>
2022-06-08 19:08:18 -0400

    first push to github

---
+++ duality/map/google-map.js
@@ -0,0 +1,48 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(42.3736, -71.1097);
+    // 39.399872
+    // -8.224454
+    
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 7,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+        styles: [
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "hue": "#ff0000"
+                    }
+                ]
+            }
+        ]
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    var addresses = [''];    
+}
+google.maps.event.addDomListener(window, 'load', init);
